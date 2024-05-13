d56ffab8c2cde86b962db7a0a6f86709e579eb68
Tahesha <taheshao@gmail.com>
2024-05-13 13:19:28 -0400

    Recreated Robin using the character class

--- JS/script.js
+++ JS/script.js
@@ -79,3 +79,13 @@ class Character {
         console.log(`${this.name} rolled a ${result}.`);
     }
 }
+
+
+//Re-create Robin using the 'character' class
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
