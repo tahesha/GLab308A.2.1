fb79d031cf50668ed8beb08e68339431d91aed2d
Tahesha <taheshao@gmail.com>
2024-05-13 12:51:27 -0400

    Added a Companion for Robin's Companion

--- JS/script.js
+++ JS/script.js
@@ -24,3 +24,18 @@ const adventurer = {
         type: "Cat"
     }
 };
+
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"],
+    companion: {
+        name: "Leo",
+        type: "Cat",
+        companion: {
+            name: "Frank",
+            type: "Flea",
+            belongings: ["small hat", "sunglasses"]
+        }
+    }
+};
