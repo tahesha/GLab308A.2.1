f9a605aca8d3c336afbcc165afdc2d5e8f59c76c
Tahesha <taheshao@gmail.com>
2024-05-13 12:58:08 -0400

    Added a method (roll)

--- JS/script.js
+++ JS/script.js
@@ -39,3 +39,23 @@ const adventurer = {
         }
     }
 };
+
+//Add a method (roll)
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"],
+    companion: {
+        name: "Leo",
+        type: "Cat",
+        companion: {
+            name: "Frank",
+            type: "Flea",
+            belongings: ["small hat", "sunglasses"]
+        }
+    },
+    roll(mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log('${this.name} rolled a ${result}.');
+    }
+};
