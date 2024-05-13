6de86e503eb62e835b719b50ee936e5b0f05a116
Tahesha <taheshao@gmail.com>
2024-05-13 12:38:48 -0400

    First commit

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+//Create the Adventurer Object. I started by defining the aventurer object with basic properties such as name, health, and inventory.
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"]
+};
